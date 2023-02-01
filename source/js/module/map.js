3347dd346d8129f8129fb7929c8a83ea6e356482
Vadim Orlov <vv-alp@mail.ru>
2023-02-02 00:55:22 +0300

    Добавлены svg logo

--- source/js/module/map.js
+++ source/js/module/map.js
@@ -3,24 +3,25 @@ function init() {
   let map = new ymaps.Map("map", {
     center: [59.94255200890565, 30.360984362273697],
     zoom: 16,
-    controls: ['default', 'routeButtonControl']
+    controls: ['zoomControl'],
+    behaviors: ['drag']}, {
+      suppressMapOpenBlock: true
   });
 
-  let placemark = new ymaps.Placemark([59.943360, 30.363489], {}, {
+  let placemark = new ymaps.Placemark([59.943360, 30.363489], {
+    hintContent: '<div class="map__hint"> ул. Радищева 39Д </div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      'Наш офис по адресу Радищева 39Д',
+      '</div>'
+    ].join('')
+  }, {
     iconLayout: 'default#image',
     iconImageHref: '../../img/icon-map-marker.svg',
     iconImageSize: [36, 36],
     iconImageOffset: [0, 0]
-  });
 
-  map.controls.remove('searchControl');
-  map.controls.remove('zoomControl');
-  map.controls.remove('geolocationControl');
-  map.controls.remove('trafficControl');
-  map.controls.remove('rulerControl');
-  map.controls.remove('routeButtonControl');
-  map.controls.remove('typeSelector');
-  map.controls.remove('fullscreenControl');
+  });
 
   map.geoObjects.add(placemark);
 }
