46b993a01843c46f048c90ae88c21c6b5aabc1bd
vad-orl <vad-orl90@mail.ru>
2023-11-03 22:07:53 +0300

    commit

---
+++ public/js/main.js
@@ -0,0 +1,66 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./source/js/app.js":
+/*!**************************!*\
+  !*** ./source/js/app.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("let navMain = document.querySelector('.main-nav');\nlet navToggle = document.querySelector('.main-nav__toggle');\n\nnavToggle.addEventListener('click', () =>{\n  if (navMain.classList.contains('main-nav--closed')) {\n    navMain.classList.remove('main-nav--closed');\n    navMain.classList.add('main-nav--opened');\n  } else {\n    navMain.classList.add('main-nav--closed');\n    navMain.classList.remove('main-nav--opened');\n  }\n});\n\n\n\n__webpack_require__(/*! ./module/map.js */ \"./source/js/module/map.js\");\n\n\n//# sourceURL=webpack://pink/./source/js/app.js?");
+
+/***/ }),
+
+/***/ "./source/js/module/map.js":
+/*!*********************************!*\
+  !*** ./source/js/module/map.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("\nfunction init() {\n  let map = new ymaps.Map(\"map\", {\n    center: [59.94255200890565, 30.360984362273697],\n    zoom: 16,\n    controls: ['zoomControl'],\n    behaviors: ['drag']}, {\n      suppressMapOpenBlock: true\n  });\n\n  let placemark = new ymaps.Placemark([59.943360, 30.363489], {\n    hintContent: '<div class=\"map__hint\"> ул. Радищева 39Д </div>',\n    balloonContent: [\n      '<div class=\"map__balloon\">',\n      'Наш офис по адресу Радищева 39Д',\n      '</div>'\n    ].join('')\n  }, {\n    iconLayout: 'default#image',\n    iconImageHref: '../../img/icon-map-marker.svg',\n    iconImageSize: [36, 36],\n    iconImageOffset: [0, 0]\n\n  });\n\n  map.geoObjects.add(placemark);\n}\n\nymaps.ready(init);\n\n\n//# sourceURL=webpack://pink/./source/js/module/map.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./source/js/app.js");
+/******/ 	
+/******/ })()
+;
